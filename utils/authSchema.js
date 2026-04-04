0cb90a6aea0efe86b5d64456816cd3f825b132ff
Hitakshi Gautam <hitakshi.gautam03@gmail.com>
2026-04-04 14:06:43 +0530

    email changes

--- utils/authSchema.js
+++ utils/authSchema.js
@@ -1,4 +1,4 @@
-import * as Yup from "yup";
+/*import * as Yup from "yup";
 const validationSchema = Yup.object().shape({
 
     email: Yup.string()
@@ -8,6 +8,23 @@ const validationSchema = Yup.object().shape({
     password: Yup.string()
       .required("Password id required")
       .min(6, "Password should be at least 6 characters long"),
+});
+
+export default validationSchema;*/
+
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required("Email is required")
+    .matches(
+      /^[a-zA-Z0-9._%+-]+@(gmail\.com|yahoo\.com|outlook\.com|hotmail\.com)$/,
+      "Enter a valid email address"
+    ),
+
+  password: Yup.string()
+    .required("Password is required")
+    .min(6, "Password should be at least 6 characters long"),
 });
 
 export default validationSchema;
